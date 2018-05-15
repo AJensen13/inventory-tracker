a913359ccf62b06daf1158970ce2c92c5dbfaa34
Andrew Jensen <jensen.andrew13@gmail.com>
2018-05-15 16:54:11 -0600

    Resolved Webpack Errors
    I changed the babel and webpack configs so that the jsx files actually transpile.

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 ReactDOM.render(<div>
   <Tooltip text="The book you're reading now">React Quickly </Tooltip>
--- webpack.config.js
+++ webpack.config.js
@@ -18,12 +18,9 @@ module.exports = {
                 ]
             },
             {
-                test: /\.jsx$/,
+                test: /\.jsx?$/,
                 exclude: /(node_modules)/,
-                use: [ { loader: 'babel-loader' } ],
-                query: {
-                    presets: ['react']
-                }
+                loader: ['babel-loader']
             }
         ]
     }
