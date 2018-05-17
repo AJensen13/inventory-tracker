f28b6d4e34e9267c1df26ef77392aa17a236b1b4
Andrew Jensen <jensen.andrew13@gmail.com>
2018-05-17 14:59:13 -0600

    Small Changes to Webpack Config
    Small changes to webpack config file for mode. I also added the app.js file to the index file and played around with some small stuff in app.jsx.

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(<div>
-  <Tooltip text="The book you're reading now">React Quickly </Tooltip>
+  React Quickly 
   was published in 2017. It's awesome!
   </div>,
   document.getElementById('app'))
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
         path: path.resolve(__dirname, 'build')
     },
     devtool: '#sourcemap',
+    mode: 'development',
     module: {
         rules: [
             {
